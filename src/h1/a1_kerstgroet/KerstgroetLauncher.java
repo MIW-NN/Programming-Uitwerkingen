b159b0d84509b15db4387cf88bcad119a93ee45c
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2025-09-05 15:03:07 +0200

    Opdracht 1.1

---
+++ src/h1/a1_kerstgroet/KerstgroetLauncher.java
@@ -0,0 +1,31 @@
+package h1.a1_kerstgroet;
+
+/**
+ * @author Vincent Velthuizen
+ * Geef een kerstige groet.
+ */
+public class KerstgroetLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("Merry Christmas!");
+        System.out.println();
+        System.out.println("    *");
+        System.out.println("  * * *");
+        System.out.println("   * *");
+        System.out.println(" * * * *");
+        System.out.println("  * * *");
+        System.out.println("* * * * *");
+        System.out.println("    *");
+
+        // Tweede kerstboom
+        System.out.println("Merry Christmas!");
+        System.out.println();
+        System.out.println("    *");
+        System.out.println("  * * *");
+        System.out.println("   * *");
+        System.out.println(" * * * *");
+        System.out.println("  * * *");
+        System.out.println("* * * * *");
+        System.out.println("    *");
+    }
+}
